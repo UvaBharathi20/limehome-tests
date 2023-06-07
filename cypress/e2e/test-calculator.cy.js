045cb2186c55acbaa36eff8c4c9e25148a501a70
UvaBharathi20 <66218352+UvaBharathi20@users.noreply.github.com>
2023-06-07 06:13:45 +0200

    test case addition.

--- cypress/e2e/test-calculator.cy.js
+++ cypress/e2e/test-calculator.cy.js
@@ -32,10 +32,26 @@ describe('Testing calculator app with positive  Test cases', () => {
     }
   })
 })
+it('test calcualtor division',() => {
+  cy.fixture('operations').then((operations) => {
+    cy.selection(operations.two[1],operations.two[0])
+  })
+  cy.fixture('inputs').then((inputs) => {
+    for (var x in inputs[0].datas.data) {
+      cy.calculate(inputs[0].datas.data[x].input1, inputs[0].datas.data[x].input2)
+        cy.get('.form-inline > .ng-binding').contains(inputs[0].datas.data[x].output[1])
+    }
+  })
 })
-  it('test calcualtor division',() => {
-
+it('test calcualtor reminder',() => {
+  cy.fixture('operations').then((operations) => {
+    cy.selection(operations.three[1],operations.three[0])
   })
-  it('test calcualtor reminder',() => {
-
+  cy.fixture('inputs').then((inputs) => {
+    for (var x in inputs[0].datas.data) {
+      cy.calculate(inputs[0].datas.data[x].input1, inputs[0].datas.data[x].input2)
+        cy.get('.form-inline > .ng-binding').contains(inputs[0].datas.data[x].output[2])
+    }
   })
+})
+})
